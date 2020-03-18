1aa2485bae5caa4a18ef5f232a71b86b3df1f4bb
Hartog de Mik <hartog.de.mik@gmail.com>
2020-03-18 20:22:43 +0100

    Extended list of questions

--- check-in.rb
+++ check-in.rb
@@ -3,7 +3,22 @@ require 'sinatra'
 get '/' do
   questions = [
     'What should you do today to make someone else happy?',
-    'What could you decide today to make tomorrow a better day?'
+    'What am I grateful for?',
+    'What do I want to learn today?',
+    'Whom would I most like to look over the shoulder?',
+    'What books are you reading?',
+    'What books should I be reading?',
+    'Try anything new lately?',
+    'Anything inspire you lately?',
+    'Seen any great design recently?',
+    'What did you do this weekend?',
+    'What am I going to do differently today?',
+    'What - realistic - dream is on your wishlist?',
+    'What do you want to do for the first time in your life?',
+    'Which exciting things, that are challenging for you, will you be doing in the next days?',
+    'Which decisions can you make today to make tomorrow a better day?',
+    'Name 3 moments/experiences from the past few days for which you are grateful',
+    'Failing to plan is planning to fail; So what could make tomorrow a great day?'
   ];
 
   return questions.shuffle.first

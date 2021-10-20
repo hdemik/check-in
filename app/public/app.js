562f83387e29a83e4e9a0ea8d69bc8730650a360
Hartog de Mik <hartog.de.mik@gmail.com>
2021-10-20 11:13:29 +0200

    Make language change functional

--- app/public/app.js
+++ app/public/app.js
@@ -3,11 +3,13 @@
 const app = new Vue({
   el: '#question',
   data: {
-    question: "..."
+    question: "…",
+    languages: { en: 'EN' },
+    language: 'en',
   },
   methods: {
     loadToday: function() {
-      return fetch('/today').then(async (res) => {
+      return fetch('/today?lang=' + this.language).then(async (res) => {
         if ( res.status === 200 ) {
           const data = await res.json();
           this.question = data.question;
@@ -15,9 +17,23 @@ const app = new Vue({
           this.question = 'failure loading question';
         }
       });
+    },
+    loadLangs: function() {
+      return fetch('/languages').then(async (res) => {
+        if ( res.status === 200 ) {
+          const data = await res.json();
+          this.languages = data.languages;
+          this.language = data.language;
+        }
+      });
+    },
+    switchLanguage: function(lang) {
+      this.language = lang;
+      this.loadToday();
     }
   },
   beforeMount: function() {
+    this.loadLangs();
     this.loadToday();
   }
 });
